b6690e93676c8e49cfa42b153abf306befc06a68
libin9ijava <libin9ijava@163.com>
2021-06-07 09:11:52 +0800

    git 版本1.0

---
+++ src/main/java/com/libin/git1/git1/Git1Application.java
@@ -0,0 +1,13 @@
+package com.libin.git1.git1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Git1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Git1Application.class, args);
+	}
+
+}
